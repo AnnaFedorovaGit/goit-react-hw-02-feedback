cd47d9551a815408828de288b00b1c34cb024638
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2023-12-16 20:00:02 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ import css from './App.module.css'
 const App = () => {
 	return (
     <div className={ css.wrapper }>
-      {/* <Profile />
-      <Statistics />
-	  <FriendList />
-      <TransactionHistory/> */}
 	</div>
 	)
 }
