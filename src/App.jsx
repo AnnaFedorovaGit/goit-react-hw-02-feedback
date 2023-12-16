a18b2066b7b78e80494c63d7537eff7c3e55ba2e
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2023-12-16 20:01:47 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import Profile from './components/Profile/Profile'
-import Statistics from './components/Statistics/Statistics'
-import FriendList from './components/FriendList/FriendList'
-import TransactionHistory from './components/TransactionHistory/TransactionHistory'
 import css from './App.module.css'
 
 const App = () => {
